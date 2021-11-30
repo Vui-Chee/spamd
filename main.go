ee0b726022440aa8ab81bce0956b3aa630aa4d83
Vui-Chee <vuicheesiew@gmail.com>
2021-11-30 22:21:16 +0800

    Test open browser after server has started

--- main.go
+++ main.go
@@ -1,28 +1,76 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
 	"os"
-
-	"bytes"
+	"os/exec"
+	"runtime"
 
 	"github.com/yuin/goldmark"
 )
 
-// TODO
-// create simple server, serving generated html from README.
+const (
+	PORT = 3001
+)
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startServer(filebytes bytes.Buffer) {
+	fmt.Printf("Starting server at port %d\n", PORT)
+	l, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", PORT))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	openbrowser(fmt.Sprintf("http://localhost:%d", PORT))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, filebytes.String())
+	})
+	// Start the blocking server loop.
+	log.Fatal(http.Serve(l, mux))
+}
+
 func main() {
-	dat, err := os.ReadFile("hello.md")
+	if len(os.Args) != 2 {
+		panic("Please enter a markdown file.")
+	}
+
+	var filename string = os.Args[1]
+
+	// Read the markdown file
+	dat, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatalln("Error reading file.")
 		os.Exit(1)
 	}
 
-	var buf bytes.Buffer
-	if err := goldmark.Convert(dat, &buf); err != nil {
+	// Parse the file data (in bytes) into a buffer.
+	var filebytes bytes.Buffer
+	if err := goldmark.Convert(dat, &filebytes); err != nil {
 		panic(err)
 	}
 
-	fmt.Println(buf.String())
+	startServer(filebytes)
 }
