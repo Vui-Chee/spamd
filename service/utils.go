63cae83d000ac84d425d38107086211799d57cb7
Vui-Chee <vuicheesiew@gmail.com>
2021-12-10 16:16:05 +0800

    Simple function to retrieve embedded bytes

--- service/utils.go
+++ service/utils.go
@@ -1,13 +1,27 @@
 package service
 
 import (
+	"embed"
 	"fmt"
 	"os"
 )
 
+//go:embed frontend
+var f embed.FS
+
 func exitOnError(msg string) {
 	if msg != "" {
 		fmt.Fprintf(os.Stderr, msg)
+		fmt.Fprintf(os.Stderr, "\n\n")
 	}
 	os.Exit(1)
+}
+
+func getEmbeddedBytes(filepath string) ([]byte, error) {
+	data, err := f.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
