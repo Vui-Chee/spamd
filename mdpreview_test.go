59e115c4403b3e733539297a45ed359ceef2f266
Vui-Chee <vuicheesiew@gmail.com>
2021-12-13 20:34:32 +0800

    Tests for isMarkdownFile()

---
+++ mdpreview_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestValidMarkdownFile(t *testing.T) {
+	cwd, _ := os.Getwd()
+
+	cases := []string{
+		"README.md",                       // just filename
+		"examples/large-readme.md",        // relative path
+		cwd + "/examples/large-readme.md", // absolute path to file
+	}
+
+	for _, filepath := range cases {
+		got := isMarkdownFile(filepath)
+		if !got {
+			t.Errorf("isMarkdownFile(\"%s\") should return true.", filepath)
+		}
+	}
+
+}
+
+func TestInvalidMarkdownFile(t *testing.T) {
+	cwd, _ := os.Getwd()
+
+	cases := []string{
+		"examples", // directory
+		".",        // relative path(s)
+		"../",
+		"foobar", // non-existent node
+		cwd,      // full path (points to root directory)
+	}
+
+	for _, filepath := range cases {
+		got := isMarkdownFile(filepath)
+		if got {
+			t.Errorf("isMarkdownFile(\"%s\") should return false.", filepath)
+		}
+	}
+}
