bc53ac85490292c5b8240ca802a4c44ff273599f
Vui-Chee <vuicheesiew@gmail.com>
2021-12-14 15:12:24 +0800

    Refactor setup/teardown methods + use temp dir

--- mdpreview_test.go
+++ mdpreview_test.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 const (
-	testdir = "tests"
+	prefix = "mdpreview-test-"
 )
 
 var testFiles = []string{
@@ -17,16 +18,16 @@ var testFiles = []string{
 
 var testSubdir = "example"
 
-func setupFiles() (bool, string) {
-	err := os.Mkdir(testdir, 0777)
+func setupFiles() (bool, string, string) {
+	testdir, err := ioutil.TempDir(".", prefix)
 	if err != nil {
-		return false, "Failed to create test directory."
+		return false, "Failed to create test directory.", ""
 	}
 
 	// Create top-level test readme file.
 	_, err = os.Create(testFiles[0])
 	if err != nil {
-		return false, fmt.Sprintf("Failed to create file %s.", testFiles[0])
+		return false, fmt.Sprintf("Failed to create file %s.", testFiles[0]), testdir
 	}
 
 	// Files created within `testdir`.
@@ -34,7 +35,7 @@ func setupFiles() (bool, string) {
 		path := fmt.Sprintf("%s/%s", testdir, file)
 		_, err := os.Create(path)
 		if err != nil {
-			return false, fmt.Sprintf("Failed to create file %s.", path)
+			return false, fmt.Sprintf("Failed to create file %s.", path), testdir
 		}
 	}
 
@@ -42,63 +43,72 @@ func setupFiles() (bool, string) {
 	path := fmt.Sprintf("%s/%s", testdir, testSubdir)
 	err = os.Mkdir(path, 0777)
 	if err != nil {
-		return false, fmt.Sprintf("Failed to create dir: %s.", path)
+		return false, fmt.Sprintf("Failed to create dir: %s.", path), testdir
 	}
 
-	return true, ""
+	return true, "", testdir
 }
 
-func setup() {
-	got, msg := setupFiles()
+func setup() (string, string) {
+	got, msg, testdir := setupFiles()
 	if !got {
-		fmt.Fprintf(os.Stderr, msg)
-		teardown()
-		os.Exit(1) // stop running test if setup failed
+		return testdir, msg
 	}
+
+	return testdir, ""
 }
 
-func teardown() {
+func teardown(testdir string) {
 	os.Remove(testFiles[0])
 	os.RemoveAll(testdir)
 }
 
+func setupAndTeardown(run func(string)) {
+	testdir, errMsg := setup()
+	if len(errMsg) > 0 {
+		fmt.Fprintf(os.Stderr, errMsg)
+		teardown(testdir)
+		os.Exit(1)
+	}
+	defer teardown(testdir)
+	run(testdir)
+}
+
 func TestValidMarkdownFile(t *testing.T) {
-	setup()
-	defer teardown()
-
-	cwd, _ := os.Getwd()
-	cases := []string{
-		testFiles[0],                 // just filename
-		testdir + "/" + testFiles[0], // relative path
-		cwd + "/" + testFiles[0],     // absolute path to file
-	}
-
-	for _, filepath := range cases {
-		got := isMarkdownFile(filepath)
-		if !got {
-			t.Errorf("isMarkdownFile(\"%s\") should return true.", filepath)
+	setupAndTeardown(func(testdir string) {
+		cwd, _ := os.Getwd()
+		cases := []string{
+			testFiles[0],                 // just filename
+			testdir + "/" + testFiles[0], // relative path
+			cwd + "/" + testFiles[0],     // absolute path to file
 		}
-	}
+
+		for _, filepath := range cases {
+			got := isMarkdownFile(filepath)
+			if !got {
+				t.Errorf("isMarkdownFile(\"%s\") should return true.", filepath)
+			}
+		}
+	})
 }
 
 func TestInvalidMarkdownFile(t *testing.T) {
-	setup()
-	defer teardown()
-
-	cwd, _ := os.Getwd()
-	cases := []string{
-		".", // relative path(s)
-		"../",
-		"foobar.md",                  // non-existent file
-		cwd,                          // full path (points to root directory)
-		testdir + "/" + testSubdir,   // directory
-		testdir + "/" + testFiles[1], // invalid extension
-	}
-
-	for _, filepath := range cases {
-		got := isMarkdownFile(filepath)
-		if got {
-			t.Errorf("isMarkdownFile(\"%s\") should return false.", filepath)
+	setupAndTeardown(func(testdir string) {
+		cwd, _ := os.Getwd()
+		cases := []string{
+			".", // relative path(s)
+			"../",
+			"foobar.md",                  // non-existent file
+			cwd,                          // full path (points to root directory)
+			testdir + "/" + testSubdir,   // directory
+			testdir + "/" + testFiles[1], // invalid extension
 		}
-	}
+
+		for _, filepath := range cases {
+			got := isMarkdownFile(filepath)
+			if got {
+				t.Errorf("isMarkdownFile(\"%s\") should return false.", filepath)
+			}
+		}
+	})
 }
