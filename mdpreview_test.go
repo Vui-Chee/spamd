56e9422bb9d2802d3501311bd55cac1ad6829e62
Vui-Chee <vuicheesiew@gmail.com>
2021-12-14 12:36:27 +0800

    Setup/teardown test files/dir

--- mdpreview_test.go
+++ mdpreview_test.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
-func TestValidMarkdownFile(t *testing.T) {
-	cwd, _ := os.Getwd()
+const (
+	testdir = "tests"
+)
 
+var testFiles = []string{
+	"TEST_README.md",
+	"go.mod",
+}
+
+var testSubdir = "example"
+
+func setupFiles() (bool, string) {
+	err := os.Mkdir(testdir, 0777)
+	if err != nil {
+		return false, "Failed to create test directory."
+	}
+
+	// Create top-level test readme file.
+	_, err = os.Create(testFiles[0])
+	if err != nil {
+		return false, fmt.Sprintf("Failed to create file %s.", testFiles[0])
+	}
+
+	// Files created within `testdir`.
+	for _, file := range testFiles {
+		path := fmt.Sprintf("%s/%s", testdir, file)
+		_, err := os.Create(path)
+		if err != nil {
+			return false, fmt.Sprintf("Failed to create file %s.", path)
+		}
+	}
+
+	// Create test sub-directory.
+	path := fmt.Sprintf("%s/%s", testdir, testSubdir)
+	err = os.Mkdir(path, 0777)
+	if err != nil {
+		return false, fmt.Sprintf("Failed to create dir: %s.", path)
+	}
+
+	return true, ""
+}
+
+func setup() {
+	got, msg := setupFiles()
+	if !got {
+		fmt.Fprintf(os.Stderr, msg)
+		teardown()
+		os.Exit(1) // stop running test if setup failed
+	}
+}
+
+func teardown() {
+	os.Remove(testFiles[0])
+	os.RemoveAll(testdir)
+}
+
+func TestValidMarkdownFile(t *testing.T) {
+	setup()
+	defer teardown()
+
+	cwd, _ := os.Getwd()
 	cases := []string{
-		"README.md",                       // just filename
-		"examples/large-readme.md",        // relative path
-		cwd + "/examples/large-readme.md", // absolute path to file
+		testFiles[0],                 // just filename
+		testdir + "/" + testFiles[0], // relative path
+		cwd + "/" + testFiles[0],     // absolute path to file
 	}
 
 	for _, filepath := range cases {
@@ -20,19 +79,20 @@ func TestValidMarkdownFile(t *testing.T) {
 			t.Errorf("isMarkdownFile(\"%s\") should return true.", filepath)
 		}
 	}
-
 }
 
 func TestInvalidMarkdownFile(t *testing.T) {
-	cwd, _ := os.Getwd()
+	setup()
+	defer teardown()
 
+	cwd, _ := os.Getwd()
 	cases := []string{
-		"examples", // directory
+		testSubdir, // directory
 		".",        // relative path(s)
 		"../",
-		"foobar", // non-existent node
-		cwd,      // full path (points to root directory)
-		"go.mod", // invalid extension
+		"foobar.md",                  // non-existent file
+		cwd,                          // full path (points to root directory)
+		testdir + "/" + testFiles[1], // invalid extension
 	}
 
 	for _, filepath := range cases {
