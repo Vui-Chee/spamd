5d7855fd88dc98cded11836470c65eb457ea957e
Vui-Chee <vuicheesiew@gmail.com>
2021-12-19 20:16:44 +0800

    Update watch service to handle multiple files

--- service/stream.go
+++ service/stream.go
@@ -10,23 +10,39 @@ import (
 	"github.com/vui-chee/mdpreview/internal/sys"
 )
 
+type fileInfo struct {
+	Lastmodifed time.Time
+	Count       int
+}
+
+// Maps a file URI to the number browser tabs
+// that open it as well as the files last modified time.
+var fileTracker = make(map[string]*fileInfo)
+
 // Need multiple channels for each connection, otherwise
 // only a single connection will be notified of any changes.
-var messageChannels = make(map[chan string]bool)
+var messageChannels = make(map[chan string]string)
 
 func refreshContent(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context().Value("filepath")
-	if ctx == nil {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("refreshContent: ctx filepath is <nil>."))
-		return
-	}
-
-	filepath := ctx.(string)
+	// Get the path relative to the directory where the tool is run.
+	// '+1' to skip the leading '/'.
+	uri := r.URL.Path
+	filepath := uri[len("/refresh")+1:]
 
 	// Create a new channel for each connection.
 	singleChannel := make(chan string)
-	messageChannels[singleChannel] = true
+	// Pass the URI as a value to allow the watcher
+	// to filter channels by file that has been modified.
+	messageChannels[singleChannel] = filepath
+
+	if _, ok := fileTracker[filepath]; ok {
+		fileTracker[filepath].Count++
+	} else {
+		fileTracker[filepath] = &fileInfo{
+			Count:       1,
+			Lastmodifed: time.Now(),
+		}
+	}
 
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Content-Type", "text/event-stream")
@@ -52,7 +68,16 @@ func refreshContent(w http.ResponseWriter, r *http.Request) {
 			w.Write(eventStreamFormat(string(content)))
 			w.(http.Flusher).Flush()
 		case <-r.Context().Done():
+			// Only decrement if key exists.
+			if _, ok := fileTracker[filepath]; ok {
+				fileTracker[filepath].Count--
+				if fileTracker[filepath].Count <= 0 {
+					delete(fileTracker, filepath)
+				}
+			}
+
 			delete(messageChannels, singleChannel)
+
 			log.Println("User closed tab. This connection is closed.")
 			return
 		}
@@ -61,26 +86,30 @@ func refreshContent(w http.ResponseWriter, r *http.Request) {
 
 func watchFile(filepath string) {
 	go func() {
-		modtime, err := sys.Modtime(filepath)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
+		// Iterate through fileTracker
+		// - for each file
+		// - check file is modified
+		// - if so, filter all channels by file -> write to the channels
 		for {
 			time.Sleep(300 * time.Millisecond) // 0.3s
 
-			newModtime, err := sys.Modtime(filepath)
-			if err != nil {
-				log.Fatal(err)
-				continue
-			}
+			for filepath, info := range fileTracker {
+				newModtime, err := sys.Modtime(filepath)
+				if err != nil {
+					log.Fatal(err)
+					continue
+				}
 
-			if modtime != newModtime {
-				fmt.Println("File was modified at: ", time.Now().Local())
-				modtime = newModtime
-				for messageChannel := range messageChannels {
-					messageChannel <- newModtime.String()
+				// How is it working when I haven't filter by filepath???
+				if info.Lastmodifed != newModtime {
+					fmt.Println("File was modified at: ", time.Now().Local())
+					info.Lastmodifed = newModtime // update modified time
+					for messageChannel, channelPath := range messageChannels {
+						// Only write to channels belonging to filepath.
+						if filepath == channelPath {
+							messageChannel <- newModtime.String()
+						}
+					}
 				}
 			}
 		}
