f75981d4bc7400a2fcc50c10901ffb5bd1ae7254
Vui-Chee <vuicheesiew@gmail.com>
2021-12-23 20:42:42 +0800

    Test additional routing check function

--- service/service_test.go
+++ service/service_test.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
 	"testing"
+
+	conf "github.com/vui-chee/mdpreview/service/config"
 )
 
 func TestListenReturnsErrOnInvalidPort(t *testing.T) {
@@ -16,5 +21,38 @@ func TestListenReturnsErrOnInvalidPort(t *testing.T) {
 		if err == nil {
 			t.Errorf("Should return error if port == %d, Got: error == nil.\n", port)
 		}
+	}
+}
+
+func TestValidRedirects(t *testing.T) {
+	var got bool
+
+	got = redirectIfNotMarkdown(conf.StylesPrefix)
+	if got != true {
+		t.Errorf("redirectIfNotMarkdown(\"%s\") should return true.", conf.StylesPrefix)
+	}
+
+	file, _ := ioutil.TempFile(".", "*.md")
+	defer os.Remove(file.Name())
+
+	uri := "/" + path.Base(file.Name())
+	got = redirectIfNotMarkdown(uri)
+	if got != true {
+		t.Errorf("redirectIfNotMarkdown(\"%s\") should return true.", uri)
+	}
+
+	// Test serving resource from refresh route.
+	uri = conf.RefreshPrefix + "/" + path.Base(file.Name())
+	got = redirectIfNotMarkdown(uri)
+	if got != true {
+		t.Errorf("redirectIfNotMarkdown(\"%s\") should return true.", uri)
+	}
+}
+
+func TestRedirectOnNoSuchFile(t *testing.T) {
+	uri := "/file-no-exists.md"
+	got := redirectIfNotMarkdown(uri)
+	if got != false {
+		t.Errorf("redirectIfNotMarkdown(\"%s\") should return false.", uri)
 	}
 }
